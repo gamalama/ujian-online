317b9e8f5bfca4424bdc30673378490c0e4de398
gamalama <fakhry.mmpp@gmail.com>
2023-10-10 11:04:17 +0800

    Membuat Helpers Dengan Mixins

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,24 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({el, App, props, plugin}) {
         createApp({render: () => h(App, props)})
+            //set mixins
+            .mixin({
+                methods: {
+
+                    examTimeRangeChecker: function (start_time, end_time) {
+                        return new Date() >= new Date(start_time) && new Date() <= new Date(end_time)
+                    },
+
+                    examTimeStartChecker: function (start_time) {
+                        return new Date() < new Date(start_time)
+                    },
+
+                    examTimeEndChecker: function (end_time) {
+                        return new Date() > new Date(end_time)
+                    }
+
+                },
+            })
             .use(plugin)
             .mount(el)
     },
