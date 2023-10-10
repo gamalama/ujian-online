dcff1fd34939e4fbb89334340c9197e512a5e547
gamalama <fakhry.mmpp@gmail.com>
2023-10-10 08:44:32 +0800

    Share Data di Inertia.js

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -19,7 +19,7 @@ class HandleInertiaRequests extends Middleware
      * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
     public function version(Request $request): ?string
@@ -31,13 +31,22 @@ class HandleInertiaRequests extends Middleware
      * Defines the props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            // session
+            'session' => [
+                'status' => fn() => $request->session()->get('status'),
+                'success' => fn() => $request->session()->get('success'),
+                'error' => fn() => $request->session()->get('error'),
+            ],
+            //user authenticated
+            'auth' => [
+                'user' => auth()->user() ? auth()->user() : null,
+            ],
         ]);
     }
 }
